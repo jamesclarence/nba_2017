8c9d79aaf28fe9a6218ac0b122e8296980a858fb
Jay Fisher <fisher.jc@gmail.com>
2016-10-19 22:45:29 -0400

    matching standings and prob try #1

---
+++ match_prob.R
@@ -0,0 +1,108 @@
+# Generate JSON file to update NBA standings and point differential
+
+# load packages
+library(rvest)
+library(stringr)
+library(jsonlite)
+library(dplyr)
+library(readr)
+
+# Set working directory
+setwd("C:/Users/fishe/Documents/nba_2017")
+
+# Read in pt_diff probability
+prob <- read_csv("probability.csv")
+
+# Read in standings from Basketball Reference
+url <- "http://www.basketball-reference.com/leagues/NBA_2016_standings.html" 
+  
+url <- read_html(url)
+
+# Clean Eastern Conference
+# Get the standings table
+east <- html_table(html_nodes(url, "table"))[[3]]
+
+# lowercase column names
+names(east) <- tolower(names(east))
+
+# remove division rows
+east <- east[-grep("Division", east$`eastern conference`),]
+ 
+# convert tm_pts and op_pts to integers
+east$tm_pts <- as.numeric(east$`ps/g`)
+east$op_pts <- as.numeric(east$`pa/g`)
+ 
+# remove pts columns
+east$`ps/g` <- NULL
+east$`pa/g` <- NULL
+
+# rename eastern conference to team
+east <- rename(east, "team" = `eastern conference`)
+
+# add conference column; add "east" to it
+east$conf <- "east"
+
+# remove asterisk from team column
+east$team <- str_replace_all(east$team, "\\*", "")
+
+# conference rank from team column to separate column
+east$conf_rank <- str_extract(east$team, "[[:space:]]\\([[:alnum:]]{1,}\\)[[:space:]]")
+east$conf_rank <- str_replace_all(east$conf_rank, "[[:punct:]]|[[:space:]]", "")
+east$conf_rank <- as.integer(east$conf_rank)
+
+# remove conference rank from team column
+# east$team <- str_replace_all(east$team, "[[:punct:]]|[[:digit:]]", "") %>% str_trim
+east$team <- str_replace_all(east$team, "[[:punct:]][[:digit:]]{1,2}[[:punct:]]", "") %>% str_trim
+
+# calculate point differential
+east <- mutate(east, pt_diff = tm_pts - op_pts)
+
+# Clean Western Conference
+# Get the standings table
+west <- html_table(html_nodes(url, "table"))[[4]]
+
+# lowercase column names
+names(west) <- tolower(names(west))
+
+# remove division rows
+west <- west[-grep("Division", west$`western conference`),]
+
+# convert tm_pts and op_pts to integers
+west$tm_pts <- as.numeric(west$`ps/g`)
+west$op_pts <- as.numeric(west$`pa/g`)
+
+# remove pts columns
+west$`ps/g` <- NULL
+west$`pa/g` <- NULL
+
+# rename western conference to team
+west <- rename(west, "team" = `western conference`)
+
+# add conference column; add "west" to it
+west$conf <- "west"
+
+# remove asterisk from team column
+west$team <- str_replace_all(west$team, "\\*", "")
+
+# conference rank from team column to separate column
+west$conf_rank <- str_extract(west$team, "[[:space:]]\\([[:alnum:]]{1,}\\)[[:space:]]")
+west$conf_rank <- str_replace_all(west$conf_rank, "[[:punct:]]|[[:space:]]", "")
+west$conf_rank <- as.integer(west$conf_rank)
+
+# remove conference rank from team column
+west$team <- str_replace_all(west$team, "[[:punct:]][[:digit:]]{1,2}[[:punct:]]", "") %>% str_trim
+
+# calculate point differential
+west <- mutate(west, pt_diff = tm_pts - op_pts)
+
+# Bind east and west conference; round pt_diff for matching
+standings <- rbind(east, west) %>% arrange(conf, conf_rank) %>% select(-gb, -srs)
+standings$pt_diff <- round(standings$pt_diff, 1)
+
+# Add in pt_diff probability
+s <- standings %>%  left_join(prob, by = c("pt_diff" = "pt_seq")) %>% filter(conf_rank.x == conf_rank.y) %>% select(-pop_mean, -pop_sd, -zscore)
+
+# Why don't pt_diff of -0.9:0.9 match?
+standings %>%  anti_join(prob, by = c("pt_diff" = "pt_seq"))
+m_rank <- match(standings$conf_rank, prob$conf_rank)
+m_ptdiff <- match(standings$pt_diff, prob$pt_seq)
